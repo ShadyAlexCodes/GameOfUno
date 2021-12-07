c8160dd076ae7a48f63983fcd2aa320b67f1711b
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2021-12-07 13:24:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gameofuno/Deck.java
@@ -0,0 +1,14 @@
+package com.example.gameofuno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+
+    private List<String> deck = new ArrayList<>();
+    private List<String> usedDeck = new ArrayList<>();
+
+
+    public void populateDeck() {
+    }
+}
---
+++ app/src/main/java/com/example/gameofuno/Player.java
@@ -0,0 +1,10 @@
+package com.example.gameofuno;
+
+public class Player {
+
+    String playerName;
+    int deckSize;
+    
+
+
+}
